46516f6a1ae07ef94cbae2906c0a21b9ea124c3f
seifa-096 <seifatef2005@gmail.com>
2025-12-01 15:59:20 +0200

    fixing bug

--- src/components/home/HeroCarousel.tsx
+++ src/components/home/HeroCarousel.tsx
@@ -1,10 +1,5 @@
 import { FC, useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import {
-  ArrowRightIcon,
-  ChevronLeftIcon,
-  ChevronRightIcon,
-} from "@heroicons/react/24/outline";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
 import { TIMING } from "@/constants";
 
 interface CarouselSlide {
